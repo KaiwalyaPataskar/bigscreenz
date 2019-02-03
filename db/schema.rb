a6f01f067e25a8bdcc3af403f544499ebd3800fe
Anuja Kunjir <anujakunjir14ak@gmail.com>
2019-02-03 18:40:59 +0530

    movies files added'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_101239) do
+ActiveRecord::Schema.define(version: 2019_02_03_111755) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "movie_name"
+    t.string "genre"
+    t.integer "rating"
+    t.datetime "release_date"
+    t.string "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
